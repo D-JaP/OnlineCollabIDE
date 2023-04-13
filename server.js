0dba77f1cf30b4459088ef5447588cf6764e84c0
dzung <dzung.leanh218@gmail.com>
2023-04-13 23:17:10 +1000

    port change

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.use(express.static(path.join(path.dirname(__dirname), '/cloudy/build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(path.dirname(__dirname) + '/cloudy/build/index.html'));
 });
-const server = app.listen(80, ()=> {
+const server = app.listen(3000, ()=> {
     console.log("login server start on port 8080")
 })
 
